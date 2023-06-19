34d519eb0a9252a0099d19b25886de1a7abc7149
cmanu07 <cemanuel86@gmail.com>
2023-06-19 09:59:14 +0300

    last commit

--- Magia_Romaniei/MODELS/ConnModel.php
+++ Magia_Romaniei/MODELS/ConnModel.php
@@ -5,7 +5,7 @@ class ConnModel
     protected $conn;
 
     public function dbConn() {        
-        $this->conn = new mysqli('localhost', 'myUser', 'ManuC531?.,', 'webdev');
+        $this->conn = new mysqli('localhost', '', '', 'webdev');
         if($this->conn->connect_error) echo 'Eroare' . $this->conn->connect_error;
         return $this->conn;
     }
